696db43bc6a3ea8c3c5f122f316a75de520897d2
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2021-02-14 22:58:30 +0900

    commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// 1. 변수 
+/*string 
+ const what = "Shin";
+ console.log(what);
+*/
+
+/*Boolean
+const wat = true;
+*/
+
+/*number
+const wat = 333;
+*/
+
+/* Float
+const wat = 55.1;
+*/
+
+
+// 2. 배열(Array)
+// const monday = "Mon";
+// const tue = "Tue";
+// const wed = "Wed";
+// const thu = "Thu";
+// const fri = "Fri";
+// console.log(monday, tue, wed, thu, fri);
+// const test = "Hello world"
+// const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", 51, true, "heyyo", test];
+// console.log(dayOfWeek);
+
+// 3. Object
+// const shinInfo = ["Shinjunghun", "41", true, "yongin"];
+
+// console.log(shinInfo);
+
+// const shinbiInfo = {
+//     name:"Shinbi",
+//     age:5,
+//     gender:"Male",
+//     isHandsome:true,
+//     FavMovie: ["spiderman", "lovegame", "ghost"],
+//     FavFood: [
+//         {
+//             name: "Kimchi",
+//             fatty: false
+//         },
+//         {
+//             name: "burger",
+//             fatty: true
+//         }
+//     ]
+// };
+
+// console.log(shinbiInfo.gender);
+
+// shinbiInfo.gender = "Female"
+
+// console.log(shinbiInfo.gender);
+
+// console.log(shinbiInfo);
+
+// console.log(shinbiInfo.FavFood);
+// console.log(shinbiInfo.FavMovie[0]);
+// console.log(shinbiInfo.age, shinbiInfo.isHandsome);
+// console.log(shinbiInfo.FavFood[0].name);
+
+4.
